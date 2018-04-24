a9d21db8d01c27846454dc307a6ce99a56dd0fc3
Zhao Zhixu <zzx.2013@qq.com>
2018-04-24 09:54:23 +0800

    add test directory and addtest.pl

---
+++ test/test_tsl.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <check.h>
+
+#include "test_tsl.h"
+
+int main(int argc, char **argv)
+{
+     int number_failed;
+     int status;
+     SRunner *sr;
+
+     sr = srunner_create(make_master_suite());
+     /* end of adding suites */
+
+     srunner_set_xml (sr, "result/check_output.xml");
+     srunner_run_all(sr, CK_NORMAL);
+     number_failed = srunner_ntests_failed(sr);
+     srunner_free(sr);
+     status = system("sed -i 's,http://check.sourceforge.net/xml/check_unittest.xslt,check_unittest.xslt,g' result/check_output.xml");
+     if (status < 0)
+          fprintf(stderr, "system() error\n");
+
+     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
---
+++ test/test_tsl.h
@@ -0,0 +1,18 @@
+#ifndef _TEST_TSL_H_
+#define _TEST_TSL_H_
+
+#include <stdio.h>
+#include <check.h>
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+Suite *make_master_suite(void);
+/* end of declarations */
+
+#ifdef _cplusplus
+}
+#endif
+
+#endif /* _TEST_TSL_H_ */
